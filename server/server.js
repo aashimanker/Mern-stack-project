2bb39c21129748b34fc528b247d491c3bd43e4e5
Aashi Manker <aashimanker3007@gmail.com>
2024-07-04 16:02:42 +0530

    created a contactForm model

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
+require('dotenv').config()
 const express = require("express")
+const errorMiddleware = require('./middlewares/error-middleware')
+
 
 const app = express()
+const authRoute = require("./router/auth-router");
+const contactRoute = require('./router/contact-router')
+const connectDb = require("./utils/db")
 
-app.get("/",(req,res)=>{
-    res.status(200).send("Welcome to my home page")
-})
+
+app.use(express.json());
+
+// Mount the router:To use the router in your express app , you can mount it at a specific url prefix
+app.use("/api/auth",authRoute)
+app.use("/api/form",contactRoute)
+
+
+//Whenever we encounter an error , we call the next() funcyion which handsover the control to the middleware for throwing an error
+app.use(errorMiddleware)
 
 const PORT = 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Our server is running at ${PORT}`)
+connectDb().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Our server is running at ${PORT}`)
+    })
 })
+
